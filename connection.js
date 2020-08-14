d8963d5d094beb778a60e856527ecc3202d0d5cb
zainja <zainjaffal@gmail.com>
2020-08-14 14:12:16 +0100

    connection modified

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 // imports
 const mysql = require('mysql')
 mysqlInfo = {
-    host: 34.89.204.130,
-    user: root,
-    password: OjDdwiJoHDrjHiAC,
-    database: ticket
+    host: "34.89.204.130",
+    user: "root",
+    password: "OjDdwiJoHDrjHiAC",
+    database: "ticket"
 }
 // connection set up
 const connection = mysql.createConnection(mysqlInfo)
